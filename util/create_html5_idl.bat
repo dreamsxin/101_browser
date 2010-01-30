05c1c45bb4380d03d26f20158c2f5025c1a5fac9
nubok <wolfgangkeller@gmail.com>
2010-01-30 18:09:04 +0100

    Added code that automatically loads the current version of the HTML5 specification.

--- util/create_html5_idl.bat
+++ util/create_html5_idl.bat
@@ -1,3 +1,5 @@
+wget http://www.whatwg.org/specs/web-apps/current-work/ -O html5.html
 java -cp htmlparser-1.2.1.jar nu.validator.htmlparser.tools.XSLT4HTML5 --template=create_idl.xslt --input-html=html5.html --output-xml=../data/idl/html5_.idl
 sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/html5_.idl > ../data/idl/html5.idl
 rm ../data/idl/html5_.idl
+rm html5.html
