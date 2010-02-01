e372ea3e3bc38303b52f13e9f3c45d7c3c4d9180
nubok <wolfgangkeller@gmail.com>
2010-02-01 17:26:03 +0100

    IDL for Web Sockets.

--- util/create_dynamic_idl.bat
+++ util/create_dynamic_idl.bat
@@ -8,6 +8,11 @@ java -cp htmlparser-1.2.1.jar nu.validator.htmlparser.tools.XSLT4HTML5 --templat
 sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/webworkers.idl
 
 
+wget http://dev.w3.org/html5/websockets/ -O websockets.html
+java -cp htmlparser-1.2.1.jar nu.validator.htmlparser.tools.XSLT4HTML5 --template=create_websockets_idl.xslt --input-html=websockets.html --output-xml=../data/idl/temp_.idl
+sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/websockets.idl
+
+
 wget http://dev.w3.org/html5/webdatabase/ -O webdatabase.html
 java -cp htmlparser-1.2.1.jar nu.validator.htmlparser.tools.XSLT4HTML5 --template=create_webdatabase_idl.xslt --input-html=webdatabase.html --output-xml=../data/idl/temp_.idl
 sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/webdatabase.idl
@@ -21,5 +26,6 @@ sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/we
 rm ../data/idl/temp_.idl
 rm html5.html
 rm webworkers.html
+rm websockets.html
 rm webdatabase.html
 rm webstorage.html
