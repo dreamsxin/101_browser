c754a08d80931ff8520ffd28e41ce6ecea8902c0
nubok <wolfgangkeller@gmail.com>
2010-02-11 00:11:00 +0100

    Some more GUI properties

--- include/gui/GuiComponentsDefaults.h
+++ include/gui/GuiComponentsDefaults.h
@@ -7,34 +7,47 @@ const float cColorByteToFloat = 1.0f/255.0f;
 
 const Color4f cColorWhite = {1.0f, 1.0f, 1.0f, 1.0f}; // (255, 255, 255)
 
-const Color4f cDefaultBackgroundColor = {
+
+const Color4f cWindowBackgroundColor = {
 	236*cColorByteToFloat, 
 	233*cColorByteToFloat, 
 	216*cColorByteToFloat, 1.0f};
 
 
-const Color4f cCheckboxTopLeftBackgroundColor = {
+const Color4f cCheckBoxTopLeftBackgroundColor = {
 	220*cColorByteToFloat, 
 	220*cColorByteToFloat, 
 	215*cColorByteToFloat, 1.0f};
-const Color4f cCheckboxTopRightBottomLeftBackgroundColor = {
+const Color4f cCheckBoxTopRightBottomLeftBackgroundColor = {
 	241*cColorByteToFloat, 
 	241*cColorByteToFloat, 
 	239*cColorByteToFloat, 1.0f}; 
-const Color4f cCheckboxBottomRightBackgroundColor = cColorWhite;
-const Color4f cCheckboxBorderColor = {
+const Color4f cCheckBoxBottomRightBackgroundColor = cColorWhite;
+const Color4f cCheckBoxBorderColor = {
 	 28*cColorByteToFloat, 
 	 81*cColorByteToFloat, 
 	128*cColorByteToFloat, 1.0f};
-const Color4f cCheckboxHookColor = {
+const Color4f cCheckBoxHookColor = {
 	 33*cColorByteToFloat, 
 	161*cColorByteToFloat, 
 	 33*cColorByteToFloat, 1.0f};
+
 
 const Color4f cTextBoxBackgroundColor = cColorWhite;
 const Color4f cTextBoxBorderColor = {
 	127*cColorByteToFloat, 
 	157*cColorByteToFloat, 
 	185*cColorByteToFloat, 1.0f};
+
+// The width and height of the checkbox (without border)
+const unsigned int cCheckBoxWidth = 11;
+const unsigned int cCheckBoxHeight = 11;
+
+// all the size is 13 - and it gets clipped to this size
+const unsigned int cRadioButtonWidth = 11;
+const unsigned int cRadioButtonHeight = 11;
+
+// The height of the textbox (without border)
+const unsigned int cTextBoxHeight = 18; 
 
 #endif
