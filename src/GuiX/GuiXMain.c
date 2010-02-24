1b6bba8c77c59106b0e7807d58851023d98d6921
Wolfgang Keller <wolfgangkeller@gmail.com>
2010-02-24 15:56:04 +0100

    Reformated source code.

--- src/GuiX/GuiXMain.c
+++ src/GuiX/GuiXMain.c
@@ -6,7 +6,6 @@
 #include<GL/glx.h>
 #include<GL/glu.h>
 
-Display                 *dpy;
 Window                  root;
 GLint                   att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
 XVisualInfo             *vi;
@@ -17,75 +16,87 @@ GLXContext              glc;
 XWindowAttributes       gwa;
 XEvent                  xev;
 
-void DrawAQuad() {
- glClearColor(1.0, 1.0, 1.0, 1.0);
- glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void DrawAQuad()
+{
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
- glMatrixMode(GL_PROJECTION);
- glLoadIdentity();
- glOrtho(-1., 1., -1., 1., 1., 20.);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1., 1., -1., 1., 1., 20.);
 
- glMatrixMode(GL_MODELVIEW);
- glLoadIdentity();
- gluLookAt(0., 0., 10., 0., 0., 0., 0., 1., 0.);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0., 0., 10., 0., 0., 0., 0., 1., 0.);
 
- glBegin(GL_QUADS);
-  glColor3f(1., 0., 0.); glVertex3f(-.75, -.75, 0.);
-  glColor3f(0., 1., 0.); glVertex3f( .75, -.75, 0.);
-  glColor3f(0., 0., 1.); glVertex3f( .75,  .75, 0.);
-  glColor3f(1., 1., 0.); glVertex3f(-.75,  .75, 0.);
- glEnd();
+	glBegin(GL_QUADS);
+	glColor3f(1., 0., 0.); glVertex3f(-.75, -.75, 0.);
+	glColor3f(0., 1., 0.); glVertex3f( .75, -.75, 0.);
+	glColor3f(0., 0., 1.); glVertex3f( .75,  .75, 0.);
+	glColor3f(1., 1., 0.); glVertex3f(-.75,  .75, 0.);
+	glEnd();
 } 
  
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+	Display                 *dpy;
+	dpy = XOpenDisplay(NULL);
 
- dpy = XOpenDisplay(NULL);
- 
- if(dpy == NULL) {
- 	printf("\n\tcannot connect to X server\n\n");
-        exit(0); }
+	if(dpy == NULL) {
+		printf("\n\tcannot connect to X server\n\n");
+       		exit(0);
+	}
         
- root = DefaultRootWindow(dpy);
+	root = DefaultRootWindow(dpy);
 
- vi = glXChooseVisual(dpy, 0, att);
+	vi = glXChooseVisual(dpy, 0, att);
 
- if(vi == NULL) {
-	printf("\n\tno appropriate visual found\n\n");
-        exit(0); } 
- else {
-	printf("\n\tvisual %p selected\n", vi->visualid); }/* %p creates hexadecimal output like in glxinfo */
+	if(vi == NULL)
+	{
+		printf("\n\tno appropriate visual found\n\n");
+        	exit(0);
+	} 
+	else
+	{
+		printf("\n\tvisual %p selected\n", vi->visualid);
+	}/* %p creates hexadecimal output like in glxinfo */
 
 
- cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
+	cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
 
- swa.colormap = cmap;
- swa.event_mask = ExposureMask | KeyPressMask;
+	swa.colormap = cmap;
+	swa.event_mask = ExposureMask | KeyPressMask;
  
- win = XCreateWindow(dpy, root, 0, 0, 600, 600, 0, vi->depth, InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
+	win = XCreateWindow(dpy, root, 0, 0, 600, 600, 0, vi->depth, InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
 
- XMapWindow(dpy, win);
- XStoreName(dpy, win, "VERY SIMPLE APPLICATION");
+	XMapWindow(dpy, win);
+	XStoreName(dpy, win, "VERY SIMPLE APPLICATION");
  
- glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
- glXMakeCurrent(dpy, win, glc);
+	glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
+	glXMakeCurrent(dpy, win, glc);
+
+	glEnable(GL_DEPTH_TEST); 
  
- glEnable(GL_DEPTH_TEST); 
- 
- while(1) {
- 	XNextEvent(dpy, &xev);
-        
-        if(xev.type == Expose) {
-        	XGetWindowAttributes(dpy, win, &gwa);
-                glViewport(0, 0, gwa.width, gwa.height);
-        	DrawAQuad(); 
-                glXSwapBuffers(dpy, win); }
-                
-	else if(xev.type == KeyPress) {
-        	glXMakeCurrent(dpy, None, NULL);
- 		glXDestroyContext(dpy, glc);
- 		XDestroyWindow(dpy, win);
- 		XCloseDisplay(dpy);
- 		exit(0); }
+	while(1)
+	{
+		XNextEvent(dpy, &xev);
+
+		if(xev.type == Expose)
+		{
+			XGetWindowAttributes(dpy, win, &gwa);
+			glViewport(0, 0, gwa.width, gwa.height);
+			DrawAQuad(); 
+			glXSwapBuffers(dpy, win);
+		}
+
+		else if(xev.type == KeyPress)
+		{
+			glXMakeCurrent(dpy, None, NULL);
+			glXDestroyContext(dpy, glc);
+			XDestroyWindow(dpy, win);
+			XCloseDisplay(dpy);
+			exit(0);
+		}
 	} /* this closes while(1) { */
 }
 
