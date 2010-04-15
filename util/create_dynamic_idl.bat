1ebc74d60d622336ffe98e2aa49d94ed3a79eb48
Wolfgang Keller <wolfgangkeller@gmail.com>
2010-04-15 02:32:16 +0200

    Created file for creating XProc grammar.

--- util/create_dynamic_idl.bat
+++ util/create_dynamic_idl.bat
@@ -30,6 +30,11 @@ java -cp htmlparser-1.2.1.jar nu.validator.htmlparser.tools.XSLT4HTML5 --templat
 sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/geolocation.idl
 
 
+wget http://www.w3.org/TR/xproc/ -O xproc.html
+java -cp htmlparser-1.2.1.jar nu.validator.htmlparser.tools.XSLT4HTML5 --template=create_xproc_grammar.xslt --input-html=xproc.html --output-xml=../data/idl/temp_.idl
+sed -e '1d' -e "s/&lt;/</" -e "s/&lt;/</" -e "s/&gt;/>/" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/web_grammar/xproc.txt
+
+
 rm ../data/idl/temp_.idl
 rm html5.html
 rm webworkers.html
@@ -37,3 +42,4 @@ rm websockets.html
 rm webdatabase.html
 rm webstorage.html
 rm geolocation.html
+rm xproc.html
