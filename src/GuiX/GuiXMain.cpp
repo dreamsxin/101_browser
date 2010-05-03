224e115c235e797401d256b5be6601208b36472c
Wolfgang Keller <wolfgangkeller@gmail.com>
2010-05-03 22:34:57 +0200

    Built (very buggy) xcb implementation of large parts.

--- src/GuiX/GuiXMain.cpp
+++ src/GuiX/GuiXMain.cpp
@@ -32,14 +32,16 @@ struct OZO_Display
 	int defaultScreen;
 	xcb_screen_t *screen;
 
-	Window rootWindow;
+	//Window rootWindow;
 };
 
 struct OZO_Window
 {
 	GLXFBConfig* FBConfig;
-	Window handle;
+	//Window handle;
+	xcb_window_t window;
 	GLXContext context;
+	GLXDrawable drawable;
 };
 
 struct OZO_Atoms
@@ -59,7 +61,7 @@ void initDisplay(OZO_Display* pDisplay, const char* displayName)
 	}
 
 	pDisplay->defaultScreen = DefaultScreen(pDisplay->display);
-	pDisplay->rootWindow = RootWindow(pDisplay->display, pDisplay->defaultScreen);
+	//pDisplay->rootWindow = RootWindow(pDisplay->display, pDisplay->defaultScreen);
 
 	pDisplay->connection = XGetXCBConnection(pDisplay->display);
 
@@ -108,12 +110,6 @@ GLXFBConfig* chooseFBConfig(const OZO_Display* pDisplay)
 	return fbconfigArray;
 }
 
-GLXContext createContext(const OZO_Display* pDisplay, const OZO_Window* pWindow)
-{
-	// this should probably not be used with OpenGL >= 3.x
-	return glXCreateNewContext(pDisplay->display, *(pWindow->FBConfig), GLX_RGBA_TYPE, NULL, True);
-}
-
 void createWindow(OZO_Display* pDisplay, OZO_Window* pWindow, const char* title, OZO_Atoms* pAtoms, 
 unsigned int width, unsigned int height)
 {
@@ -126,47 +122,8 @@ unsigned int width, unsigned int height)
 	}
 	XVisualInfo * visualInfo = glXGetVisualFromFBConfig(pDisplay->display, *(pWindow->FBConfig));
 
-	XSetWindowAttributes winAttr;
-
-	winAttr.background_pixmap = None;
-	winAttr.background_pixel  = 0;
-	winAttr.border_pixel      = 0;
-	winAttr.colormap = XCreateColormap(
-		pDisplay->display, pDisplay->rootWindow,
-		visualInfo->visual, AllocNone
-	);
-
-	winAttr.event_mask = StructureNotifyMask | SubstructureNotifyMask | ExposureMask | 
-		ButtonPressMask | ButtonReleaseMask | KeyPressMask | KeyReleaseMask |
-		VisibilityChangeMask | EnterWindowMask | LeaveWindowMask |
-		PointerMotionMask | ButtonMotionMask;
-
-	/*
-	 * this tells us which fields of winAttr we have set
-	 */
-	long mask = CWBackPixmap | CWBorderPixel | CWColormap | CWEventMask;
-
-	int x = -1;
-	int y = -1;
-
-	pWindow->handle = XCreateWindow(
-		pDisplay->display,
-		/*
-		 * In freeglut we have:
-		 * pWindow->parent == NULL ? pDisplay->rootWindow : pWindow->parent->handle
-		 * 
-		 * In future we could perhaps use that?
-		 */
-		pDisplay->rootWindow, // parent window
-		x, y, (unsigned int) width, (unsigned int) height,
-		0,                    // border width
-		visualInfo->depth,    // depth
-		InputOutput,          // class
-		visualInfo->visual,
-		mask,                 // which values are defined in winAttr
-		&winAttr);
-
-	pWindow->context = createContext(pDisplay, pWindow);
+	// this should probably not be used with OpenGL >= 3.x
+	pWindow->context = glXCreateNewContext(pDisplay->display, *(pWindow->FBConfig), GLX_RGBA_TYPE, NULL, True);
 
 	if (!pWindow->context)
 	{
@@ -175,10 +132,84 @@ unsigned int width, unsigned int height)
 		exit(1);
 	}
 
+	//XSetWindowAttributes winAttr;
+	//winAttr.background_pixmap = None;
+	uint32_t back_pixmap = XCB_BACK_PIXMAP_NONE;
+	//winAttr.background_pixel  = 0;
+	uint32_t background_pixel = 0;
+	//winAttr.border_pixel      = 0;
+	uint32_t border_pixel = 0;
+
+	//winAttr.colormap = XCreateColormap(
+	//	pDisplay->display, pDisplay->rootWindow,
+	//	visualInfo->visual, AllocNone
+	//);
+	xcb_colormap_t colormap = xcb_generate_id(pDisplay->connection);
+	xcb_create_colormap(
+		pDisplay->connection,
+		XCB_COLORMAP_ALLOC_NONE,
+		colormap,
+		pDisplay->screen->root,
+		visualInfo->visualid);
+
+	//winAttr.event_mask = StructureNotifyMask | SubstructureNotifyMask | ExposureMask | 
+	//	ButtonPressMask | ButtonReleaseMask | KeyPressMask | KeyReleaseMask |
+	//	VisibilityChangeMask | EnterWindowMask | LeaveWindowMask |
+	//	PointerMotionMask | ButtonMotionMask;
+	uint32_t event_mask = XCB_EVENT_MASK_STRUCTURE_NOTIFY | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_EXPOSURE | 
+		XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE |
+		XCB_EVENT_MASK_VISIBILITY_CHANGE | XCB_EVENT_MASK_ENTER_WINDOW | XCB_EVENT_MASK_LEAVE_WINDOW |
+		XCB_EVENT_MASK_POINTER_MOTION | XCB_EVENT_MASK_BUTTON_MOTION;
+
+	/*
+	 * this tells us which fields of winAttr we have set
+	 */
+	//long mask = CWBackPixmap | CWBorderPixel | CWEventMask | CWColormap;
+	uint32_t valuemask = XCB_CW_BACK_PIXMAP | XCB_CW_BORDER_PIXEL | XCB_CW_EVENT_MASK | XCB_CW_COLORMAP;
+	uint32_t valuelist[] = { 0, 0, event_mask, colormap };
+
+	//int x = -1;
+	//int y = -1;
+	int16_t x = -1;
+	int16_t y = -1;
+		
+
+	//pWindow->handle = XCreateWindow(
+	//	pDisplay->display,
+		/*
+		 * In freeglut we have:
+		 * pWindow->parent == NULL ? pDisplay->rootWindow : pWindow->parent->handle
+		 * 
+		 * In future we could perhaps use that?
+		 */
+	//	pDisplay->rootWindow, // parent window
+	//	x, y, (unsigned int) width, (unsigned int) height,
+	//	0,                    // border width
+	//	visualInfo->depth,    // depth
+	//	InputOutput,          // class
+	//	visualInfo->visual,
+	//	mask,                 // which values are defined in winAttr
+	//	&winAttr);
+	pWindow->window = xcb_generate_id(pDisplay->connection);
+	xcb_create_window(
+		pDisplay->connection, 
+		visualInfo->depth,             // depth
+		pWindow->window,
+		pDisplay->screen->root,
+		x, y, (uint16_t) width, (uint16_t) height,
+		0,                             // border width
+		XCB_WINDOW_CLASS_INPUT_OUTPUT, // class
+		visualInfo->visualid,
+		valuemask,
+		valuelist
+	);
+
+	XFree(visualInfo);
+
 	bool positionUse = false;
 	bool sizeUse = true;
 	
-	XSizeHints sizeHints;
+	/*XSizeHints sizeHints;
 	sizeHints.flags = 0;
     	if (positionUse)
         	sizeHints.flags |= USPosition;
@@ -210,24 +241,56 @@ unsigned int width, unsigned int height)
 	XFree(textProperty.value);
 
 	pAtoms->deleteWindow = XInternAtom(pDisplay->display, "WM_DELETE_WINDOW", True);
-	XSetWMProtocols(pDisplay->display, pWindow->handle, &pAtoms->deleteWindow, 1);
+	XSetWMProtocols(pDisplay->display, pWindow->handle, &pAtoms->deleteWindow, 1);*/
 
+	//XMapWindow(pDisplay->display, pWindow->handle);
+	xcb_map_window(pDisplay->connection, pWindow->window);
+
+	GLXWindow glxwindow = glXCreateWindow(
+                pDisplay->display,
+                *(pWindow->FBConfig),
+                pWindow->window,
+                NULL
+                );
+
+	if (!glxwindow)
+	{
+		fprintf(stderr, "glXCreateWindow failed\n");
+		cleanup(pDisplay, pWindow);
+		exit(1);
+	}
+
+	pWindow->drawable = glxwindow;
+
+	//glXMakeContextCurrent(
+	//	pDisplay->display,
+	//	pWindow->handle,    // GLX drawable to render into
+	//	pWindow->handle,    // GLX drawable to read from
+	//	pWindow->context
+	//);
 	glXMakeContextCurrent(
 		pDisplay->display,
-		pWindow->handle,    // GLX drawable to render into
-		pWindow->handle,    // GLX drawable to read from
+		glxwindow,    // GLX drawable to render into
+		glxwindow,    // GLX drawable to read from
 		pWindow->context
 	);
-
-	XMapWindow(pDisplay->display, pWindow->handle);
-
-	XFree(visualInfo);
 }
 
 void cleanup(OZO_Display* pDisplay, OZO_Window* pWindow)
 {
 	if (pWindow)
 	{
+		if (pWindow->drawable)
+		{
+			glXDestroyWindow(pDisplay->display, pWindow->drawable);
+		}
+
+		if (pWindow->window)
+		{
+			xcb_destroy_window(pDisplay->connection, pWindow->window);
+			memset(&pWindow->window, sizeof(pWindow->window), 0);
+		}
+
 		if (pWindow->context)
 		{
 			glXDestroyContext(pDisplay->display, pWindow->context);
@@ -238,12 +301,6 @@ void cleanup(OZO_Display* pDisplay, OZO_Window* pWindow)
 		{
 			XFree(pWindow->FBConfig);
 			memset(&pWindow->FBConfig, sizeof(pWindow->FBConfig), 0);
-		}
-
-		if (pWindow->handle)
-		{
-			XDestroyWindow(pDisplay->display, pWindow->handle);
-			memset(&pWindow->handle, sizeof(pWindow->handle), 0);
 		}
 	}
 
@@ -257,18 +314,21 @@ void cleanup(OZO_Display* pDisplay, OZO_Window* pWindow)
 	}
 }
 
-void printEvent(int type)
+//void printEvent(int type)
+void printEvent(uint8_t response_type)
 {
-	switch(type)
+	switch(response_type)
 	{
-	case KeyPress:
-		printf("KeyPress\n");
+	//case KeyPress:
+	case XCB_KEY_PRESS:
+		printf("XCB_KEY_PRESS\n");
 		break;
-	case ClientMessage:
-		printf("ClientMessage\n");
+	//case ClientMessage:
+	case XCB_CLIENT_MESSAGE:
+		printf("XCB_CLIENT_MESSAGE\n");
 		break;
 	default:
-		printf("Unknown event with value %u\n", type);
+		printf("Unknown event with value %u\n", response_type);
 		break;
 	}
 }
@@ -277,7 +337,7 @@ void draw(const OZO_Display* pDisplay, const OZO_Window* pWindow)
 {
 	UpdateGuiState();
 	drawGui();
-	glXSwapBuffers(pDisplay->display, pWindow->handle);
+	glXSwapBuffers(pDisplay->display, pWindow->drawable);
 }
 
 void mainLoop(OZO_Display* pDisplay, const OZO_Window* pWindow, OZO_Atoms* pAtoms, int *currentWidth, int *currentHeight)
@@ -285,39 +345,49 @@ void mainLoop(OZO_Display* pDisplay, const OZO_Window* pWindow, OZO_Atoms* pAtom
 	bool done = false;
 
 	while (!done)
-	{	
-		while (XPending(pDisplay->display))
+	{
+		xcb_generic_event_t *ev;
+
+		//while (XPending(pDisplay->display))
+		while (ev = xcb_poll_for_event(pDisplay->connection))
 		{
-			XEvent event;
-			XNextEvent(pDisplay->display, &event);
+			//XEvent event;
+			//XNextEvent(pDisplay->display, &event);
 
-			printEvent(event.type);
+			//printEvent(event.type);
+			printEvent(ev->response_type);
 
-			switch(event.type)
+			//switch (event.type)
+			switch (ev->response_type)
 			{
-			case KeyPress:
-				if (XLookupKeysym(&event.xkey, 0) == XK_Escape)
+			//case KeyPress:
+			case XCB_KEY_PRESS:
+				//if (XLookupKeysym(&event.xkey, 0) == XK_Escape)
+				break;
+			//case ClientMessage:
+			case XCB_CLIENT_MESSAGE:
+			/*	if((Atom) event.xclient.data.l[0] == pAtoms->deleteWindow)
 				{
 					done = true;
-                    		}
+				}*/
 				break;
-			case ClientMessage:
-				if((Atom) event.xclient.data.l[0] == pAtoms->deleteWindow)
-				{
-					done = true;
-				}
-				break;
-			case CreateNotify:
-        		case ConfigureNotify:
+			//case CreateNotify:
+			case XCB_CREATE_NOTIFY:
+        		//case ConfigureNotify:
+			case XCB_CONFIGURE_NOTIFY:
 			{
-				int newWidth, newHeight;
-				if (event.type == CreateNotify) {
-					newWidth = event.xcreatewindow.width;
-					newHeight = event.xcreatewindow.height;
+				uint16_t newWidth, newHeight;
+				if (ev->response_type == XCB_CREATE_NOTIFY) {
+					//newWidth = event.xcreatewindow.width;
+					//newHeight = event.xcreatewindow.height;
+					newWidth = ((xcb_create_notify_event_t *) ev)->width;
+					newHeight = ((xcb_create_notify_event_t *) ev)->height;
                 		} else {
-					assert(event.type == ConfigureNotify);
-					newWidth = event.xconfigure.width;
-					newHeight = event.xconfigure.height;
+					assert(ev->response_type == XCB_CONFIGURE_NOTIFY);
+					//newWidth = event.xconfigure.width;
+					//newHeight = event.xconfigure.height;
+					newWidth = ((xcb_configure_notify_event_t *) ev)->width;
+					newHeight = ((xcb_configure_notify_event_t *) ev)->height;
 				}
 
 				if ((*currentWidth != newWidth) || (*currentHeight != newHeight))
