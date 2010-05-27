546ddf8a34d43ec024fcf91b47b04b338dd2f2cf
Wolfgang Keller <wolfgangkeller@gmail.com>
2010-05-27 16:45:35 +0200

    Fixed compilation bug under Linux.

--- include/BasicDataStructures/Error/ErrorHandling.h
+++ include/BasicDataStructures/Error/ErrorHandling.h
@@ -11,6 +11,16 @@ inline void showErrorMessage(const wchar_t* const in_message)
 	MessageBox(HWND_DESKTOP, in_message, L"Error", MB_OK | MB_ICONEXCLAMATION);
 }
 
+#else
+
+#include <cwchar>
+#include <cstdio>
+
+inline void showErrorMessage(const wchar_t* const in_message)
+{
+	fwprintf(stderr, L"Error: %s\n", in_message);
+}
+
 #endif
 
 inline void showErrorMessageAndExit(const wchar_t* const in_message)
