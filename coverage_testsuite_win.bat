7d91e98f2c3707fed3cd889fa4da4c9878c1642d
Wolfgang Keller <wolfgangkeller@gmail.com>
2010-08-04 16:00:13 +0200

    Fixed another bug in coverage_testsuite_win.bat (using a file that is not in PATH).

--- coverage_testsuite_win.bat
+++ coverage_testsuite_win.bat
@@ -1,4 +1,7 @@
+SET PATH=%VSINSTALLDIR%\Team Tools\Performance Tools\;%PATH%
+
 vsinstr out\vs2008\Debug\TestSuite.exe /COVERAGE
+vsinstr out\vs2008\Debug\BigNumber.dll /COVERAGE
 Start VSPerfMon.exe /COVERAGE /OUTPUT:testsuite
 PAUSE
 out\vs2008\Debug\TestSuite.exe
