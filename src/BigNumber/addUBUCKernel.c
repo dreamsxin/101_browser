d976da6bab05d1e19f784bc10420b93e820cccbf
Wolfgang Keller <wolfgangkeller@gmail.com>
2010-09-05 18:23:14 +0200

    Removed the assembler version since it was slower. :-(

--- src/BigNumber/addUBUCKernel.c
+++ src/BigNumber/addUBUCKernel.c
@@ -2,23 +2,23 @@
 
 bool addUBUCKernel(uint32_t* in_pCarry, uint32_t* in_pCurrentDigit)
 {
-#ifdef _M_IX86
-	__asm
-	{
-		mov edx, dword ptr [in_pCurrentDigit]
-		mov ecx, dword ptr [in_pCarry]
-
-		mov eax, dword ptr [ecx]
-		add dword ptr [edx], eax
-
-		setc al
-		mov byte ptr [ecx], al
-	}
-#else
+	/*!
+	 * Notice:
+	 * this function can be represented in a very elegant (but slower - why?) way
+	 * by the following assembler code:
+	 * 
+	 *     mov edx, dword ptr [in_pCurrentDigit]
+	 *     mov ecx, dword ptr [in_pCarry]
+	 * 
+	 *     mov eax, dword ptr [ecx]
+	 *     add dword ptr [edx], eax
+	 * 
+	 *     setc al
+	 *     mov byte ptr [ecx], al
+	 */
 	uint64_t sum = ((uint64_t) *in_pCarry) + ((uint64_t) *in_pCurrentDigit);
 	*in_pCurrentDigit = (uint32_t) sum;
 	*in_pCarry = (uint32_t) (sum >> 32);
 
 	return (*in_pCarry != 0);
-#endif
 }
