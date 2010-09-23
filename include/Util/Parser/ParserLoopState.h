1c835434496c86cd1673b4fe6500d59f9426272d
Wolfgang Keller <wolfgangkeller@gmail.com>
2010-09-23 18:09:03 +0200

    New header file for parser loop state. The XSLT parser generator now also writes initialization code.

---
+++ include/Util/Parser/ParserLoopState.h
@@ -0,0 +1,11 @@
+#ifndef _ParserLoopState_h
+#define _ParserLoopState_h
+
+enum ParserLoopState
+{
+	ParserLoopStateContinueLoop,
+	ParserLoopStateBreakLoopSuccess;
+	ParserLoopStateBreakLoopFailure;
+}
+
+#endif
