5bba659ab5d6bbf6dd7d325ff2e366b32ce5e39b
Wolfgang Keller <wolfgangkeller@gmail.com>
2010-12-13 22:08:25 +0100

    Beginning of code for reading chunks.

--- src/PNG/PNG.c
+++ src/PNG/PNG.c
@@ -1,5 +1,6 @@
 #include "PNG/PNG.h"
 #include <string.h>
+#include <stddef.h> // for offsetof
 
 // See http://www.w3.org/TR/PNG/#5PNG-file-signature
 const uint8_t PNG_signature[8] = { 137, 80, 78, 71, 13, 10, 26, 10 };
@@ -19,5 +20,10 @@ ReadResult read_PNG(FILE* in_pngFile)
 
 ReadResult read_PNG_Chunk(PNG_Chunk *out_pPNG_Chunk, FILE* in_pngFile)
 {
+	if (fread(out_pPNG_Chunk, offsetof(PNG_Chunk, chunkData), 1, in_pngFile) != 1)
+	{
+		return ReadResultPrematureEndOfStream;
+	}
+	
 	return ReadResultNotImplemented;
 }
