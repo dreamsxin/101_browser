48fac984b36414946747037a67366025b7b70cae
Wolfgang Keller <wolfgangkeller@gmail.com>
2010-12-28 13:38:16 +0100

    Beginning of a general stream interface.

---
+++ include/IO/ByteStreamInterface.h
@@ -0,0 +1,28 @@
+#ifndef _MTAx_IO_ByteStreamInterface_h
+#define _MTAx_IO_ByteStreamInterface_h
+
+/* 
+ * gcc requires this header file for a definition of size_t
+ */
+#ifdef __GNUC__
+# ifdef __cplusplus
+#include <cstddef>
+# else
+#include <stddef.h>
+# endif
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct
+{
+	size_t (*pRead)(void *in_out_pByteStream, void *in_pBuffer, size_t in_count);
+} ByteStreamInterface;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
