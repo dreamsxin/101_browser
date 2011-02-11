d0f56fa744131adc4ad54405e54fbd621949e42b
Wolfgang Keller <wolfgangkeller@gmail.com>
2011-02-11 23:30:39 +0100

    Added licence header to Bitmask.

--- include/Algorithm/Bitmask.h
+++ include/Algorithm/Bitmask.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Wolfgang Keller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef _MTAx_Algorithm_Bitmask_h
 #define _MTAx_Algorithm_Bitmask_h
 
@@ -8,6 +24,9 @@
 extern "C" {
 #endif
 
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
 bool checkBitmask(const void *in_pBuffer, size_t in_bufferSize, const void *in_pMaskIdentifierBits, const void *in_pMaskValueBits);
 
 #ifdef __cplusplus
--- src/Algorithm/Bitmask.c
+++ src/Algorithm/Bitmask.c
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2011 Wolfgang Keller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "Algorithm/Bitmask.h"
 #include "MiniStdlib/cstddef.h" // for SIZE_MAX on Unix
 #include <assert.h>
 
-bool checkBitmask(void *in_pBuffer, size_t in_bufferSize, const void *in_pMaskIdentifierBits, const void *in_pMaskValueBits)
+bool checkBitmask(const void *in_pBuffer, size_t in_bufferSize, const void *in_pMaskIdentifierBits, const void *in_pMaskValueBits)
 {
 	const uint8_t *pBuffer = (const uint8_t*) in_pBuffer;
 	const uint8_t *pMaskIdentifierBits = (const uint8_t*) in_pMaskIdentifierBits;
