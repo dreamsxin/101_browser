14bf6015d61d97b857f2e3553381041d57c3e798
Wolfgang Keller <wolfgangkeller@gmail.com>
2011-03-30 22:13:27 +0200

    Removed outdated files. Moved generated files.

--- util/create_dynamic_idl.bat
+++ util/create_dynamic_idl.bat
@@ -7,37 +7,37 @@ wget http://unicode.org/Public/UNIDATA/PropList.txt -O ../data/Unicode/PropList.
 
 wget http://www.whatwg.org/specs/web-apps/current-work/ -O html.html
 java -cp htmlparser-1.2.1.jar nu.validator.htmlparser.tools.XSLT4HTML5 --template=create_html_idl.xslt --input-html=html.html --output-xml=../data/idl/temp_.idl
-sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/html.idl
+sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/generated/html.idl
 
 
 wget http://www.whatwg.org/specs/web-workers/current-work/ -O webworkers.html
 java -cp htmlparser-1.2.1.jar nu.validator.htmlparser.tools.XSLT4HTML5 --template=create_webworkers_idl.xslt --input-html=webworkers.html --output-xml=../data/idl/temp_.idl
-sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/webworkers.idl
+sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/generated/webworkers.idl
 
 
 wget http://dev.w3.org/html5/websockets/ -O websockets.html
 java -cp htmlparser-1.2.1.jar nu.validator.htmlparser.tools.XSLT4HTML5 --template=create_websockets_idl.xslt --input-html=websockets.html --output-xml=../data/idl/temp_.idl
-sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/websockets.idl
+sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/generated/websockets.idl
 
 
 wget http://dev.w3.org/html5/webdatabase/ -O webdatabase.html
 java -cp htmlparser-1.2.1.jar nu.validator.htmlparser.tools.XSLT4HTML5 --template=create_webdatabase_idl.xslt --input-html=webdatabase.html --output-xml=../data/idl/temp_.idl
-sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/webdatabase.idl
+sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/generated/webdatabase.idl
 
 
 wget http://dev.w3.org/html5/webstorage/ -O webstorage.html
 java -cp htmlparser-1.2.1.jar nu.validator.htmlparser.tools.XSLT4HTML5 --template=create_webstorage_idl.xslt --input-html=webstorage.html --output-xml=../data/idl/temp_.idl
-sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/webstorage.idl
+sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/generated/webstorage.idl
 
 
 wget http://dev.w3.org/geo/api/spec-source.html -O geolocation.html
 java -cp htmlparser-1.2.1.jar nu.validator.htmlparser.tools.XSLT4HTML5 --template=create_geolocation_idl.xslt --input-html=geolocation.html --output-xml=../data/idl/temp_.idl
-sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/geolocation.idl
+sed -e '1d' -e "s/&lt;/</" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/idl/generated/geolocation.idl
 
 
 wget http://www.w3.org/TR/xproc/ -O xproc.html
 java -cp htmlparser-1.2.1.jar nu.validator.htmlparser.tools.XSLT4HTML5 --template=create_xproc_grammar.xslt --input-html=xproc.html --output-xml=../data/idl/temp_.idl
-sed -e '1d' -e "s/&lt;/</" -e "s/&lt;/</" -e "s/&gt;/>/" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/web_grammar/xproc.txt
+sed -e '1d' -e "s/&lt;/</" -e "s/&lt;/</" -e "s/&gt;/>/" -e "s/&gt;/>/" ../data/idl/temp_.idl > ../data/web_grammar/generated/xproc.txt
 
 
 rm ../data/idl/temp_.idl
