ce065183266c4b67df3d4ae21569cf33df297fdc
Wolfgang Keller <wolfgangkeller@gmail.com>
2011-03-31 01:37:07 +0200

    Removed pstdint.h since not used anymore.

--- include/MiniStdlib/cstdint.h
+++ include/MiniStdlib/cstdint.h
@@ -18,24 +18,13 @@
 #define _MTAx_MiniStdlib_cstdint_h
 
 # ifdef __cplusplus
-/*
- * Visual Studio 2010 is the first version to 
- * contain cstdint. In the versions before we
- * have to use boost. Also gcc does not like this
- */
-#  if defined(_MSC_VER) && (_MSC_VER < 1600)
-#include "contrib/pstdint.h"
-#  elif defined(__GNUC__)
-#include <stdint.h>
+#  if defined(__GNUC__)
+#   include <stdint.h>
 #  else
-#include <cstdint>
+#   include <cstdint>
 #  endif
 # else
-#  if defined(_MSC_VER) && (_MSC_VER < 1600)
-#include "contrib/pstdint.h"
-#  else
-#include <stdint.h>
-#  endif
+#   include <stdint.h>
 # endif
 
 #endif
