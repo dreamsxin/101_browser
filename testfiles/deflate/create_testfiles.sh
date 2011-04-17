484dea2007bb00e230d10d6f8ec0bfd10e4d6932
Wolfgang Keller <wolfgangkeller@gmail.com>
2011-04-18 00:37:53 +0200

    New testfile

--- testfiles/deflate/create_testfiles.sh
+++ testfiles/deflate/create_testfiles.sh
@@ -11,7 +11,13 @@ mv random0.raw null.raw
 # The gzip is not able to make a file of 65536 random bytes using only 
 # one block. So we have to hack a manual solution...
 dd if=/dev/urandom of="random65535" bs=65535 count=1
+# -n: no newline at the end
 echo -n -e "\x01\xff\xff\x00\x00" > random65535.raw
 cat random65535 >> random65535.raw
 
+# At the moment, we don't want this test file, that's why it is commented
+# cat null.raw null.raw > error_data_after_final_block.raw
 
+# 02 00 - the same as null.raw, but single block is not marked
+# as final (03 00 as in null.raw - here the block is marked as final
+echo -n -e "\x02\x00" > error_no_final_block.raw
