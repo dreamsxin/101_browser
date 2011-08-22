33d2891ea13245dd869623216b3e9d033da76e90
Wolfgang Keller <wolfgangkeller@gmail.com>
2011-08-22 15:46:33 +0200

    Removed comments (explanations can be found on imagetestsuite project).

--- src/TestSuite/Test_GIF.c
+++ src/TestSuite/Test_GIF.c
@@ -61,65 +61,23 @@ char* filenames[] = {
 
 Char96ReadResultPair filenameResults[] = 
 {
-	/*
-	* Some block beginning with 0x00.
-	*/
 	{"testfiles/imagetestsuite/gif/0646caeb9b9161c777f117007921a687.gif", ReadResultInvalidData}, 
-	/*
-	* Invalid since the block size of the Graphic Control Extension
-	* block is 0xEE instead of 0x04.
-	*/
 	{"testfiles/imagetestsuite/gif/243d9798466d64aba0acaa41f980bea6.gif", ReadResultInvalidData}, 
-	/*
-	* Invalid since minimum code size = 0x48 (instead of between 2 and 8)
-	*/
 	{"testfiles/imagetestsuite/gif/2b5bc31d84703bfb9f371925f0e3e57d.gif", ReadResultInvalidData}, 
 	{"testfiles/imagetestsuite/gif/55abb3cc464305dd554171c3d44cb61f.gif", ReadResultInvalidData}, 
-	/*
-	* Invalid since minimum code size = 0x48 (instead of between 2 and 8)
-	*/
 	{"testfiles/imagetestsuite/gif/5f09a896c191db3fa7ea6bdd5ebe9485.gif", ReadResultInvalidData}, 
 	{"testfiles/imagetestsuite/gif/6d939393058de0579fca1bbf10ecff25.gif", ReadResultPrematureEndOfStream}, 
-	/*
-	* Invalid since the block size of the Graphic Control Extension
-	* block is 0xEE instead of 0x04.
-	*/
 	{"testfiles/imagetestsuite/gif/7092f253998c1b6b869707ad7ae92854.gif", ReadResultInvalidData}, 
 	{"testfiles/imagetestsuite/gif/9f8f6046eaf9ffa2d9c5d6db05c5f881.gif", ReadResultInvalidData}, 
 	{"testfiles/imagetestsuite/gif/adaf0da1764aafb7039440dbe098569b.gif", ReadResultPrematureEndOfStream}, 
-	/*
-	* Invalid since there is a code word currentCodeWord where (currentCodeWord >= currentTableIndex)
-	*/
 	{"testfiles/imagetestsuite/gif/adf6f850b13dff73ebb22862c6ab028b.gif", ReadResultInvalidData}, 
-	/*
-	* Application Extension block directly after Graphics Control Extension
-	*/
 	{"testfiles/imagetestsuite/gif/bc7af0616c4ae99144c8600e7b39beea.gif", ReadResultInvalidData}, 
-	/*
-	* Invalid since minimum code size = 0x0 (instead of between 2 and 8)
-	*/
 	{"testfiles/imagetestsuite/gif/ce774930ac70449f38a18789c70095b8.gif", ReadResultInvalidData}, 
-	/*
-	* Signature 0x00 0x49 0x46 instead of 0x47 0x49 0x46 (if we correct 
-	* it, we still have an invalid file since
-	* minimum code size = 0x48 (instead of between 2 and 8))
-	*/
 	{"testfiles/imagetestsuite/gif/d5a0175c07418852152ef33a886a5029.gif", ReadResultInvalidData}, 
 	{"testfiles/imagetestsuite/gif/e34116d68f49c7852b362ec72a636df5.gif", ReadResultPrematureEndOfStream}, 
 	{"testfiles/imagetestsuite/gif/e6aa0c45a13dd7fc94f7b5451bd89bf4.gif", ReadResultOK},
 	{"testfiles/imagetestsuite/gif/ea754e040929b7f9c157efc88c4d0eaf.gif", ReadResultPrematureEndOfStream}, 
-
-	/*
-	* GIF 87a image contains a Graphic Control Extension although this
-	* extension does only exist from version 89a on. So it has to be 
-	* skipped.
-	* Additionally there is one more byte in the Image Data block (0x2B)
-	* than necessary (only reading 0x2A bytes).
-	*/
 	{"testfiles/imagetestsuite/gif/ee6d1133f9264dc6467990e53d0bf104.gif", ReadResultInvalidData},
-	/*
-	* Application Extension block directly after Graphics Control Extension
-	*/
 	{"testfiles/imagetestsuite/gif/f617c7af7f36296a37ddb419b828099c.gif", ReadResultInvalidData}, 
 	{"testfiles/imagetestsuite/gif/f88b6907ee086c4c8ac4b8c395748c49.gif", ReadResultInvalidData},
 	{"testfiles/imagetestsuite/gif/fc3e2b992c559055267e26dc23e484c0.gif", ReadResultInvalidData}
