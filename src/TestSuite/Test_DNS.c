bf9cd6611d4bd0459ba6b2e9d5e6c753d7b9cd75
Wolfgang Keller <wolfgangkeller@gmail.com>
2012-02-15 02:59:39 +0100

    Updated copyright information.

--- src/TestSuite/Test_DNS.c
+++ src/TestSuite/Test_DNS.c
@@ -1,5 +1,5 @@
 /*
-* Copyright 2011 Wolfgang Keller
+* Copyright 2011-2012 Wolfgang Keller
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
