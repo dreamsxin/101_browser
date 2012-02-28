d9b15a49a29cc64ab26e72faf8597e6647a344d1
Wolfgang Keller <wolfgangkeller@gmail.com>
2012-02-29 00:44:59 +0100

    Update in coverage test script.

--- coverage_testsuite_win_vs2010.bat
+++ coverage_testsuite_win_vs2010.bat
@@ -1,3 +1,4 @@
+set OLDPATH=%PATH%
 SET PATH=%VSINSTALLDIR%\Team Tools\Performance Tools\;%PATH%
 
 vsinstr out\vs2010\Debug\TestSuite.exe /COVERAGE
@@ -16,3 +17,14 @@ PAUSE
 out\vs2010\Debug\TestSuite.exe
 PAUSE
 VSPerfCmd.exe /SHUTDOWN
+
+move out\vs2010\Debug\TestSuite.exe.orig out\vs2010\Debug\TestSuite.exe
+move out\vs2010\Debug\Algorithm.dll.orig out\vs2010\Debug\Algorithm.dll
+move out\vs2010\Debug\BigNumber.dll.orig out\vs2010\Debug\BigNumber.dll
+move out\vs2010\Debug\DNS.dll.orig out\vs2010\Debug\DNS.dll
+move out\vs2010\Debug\GIF.dll.orig out\vs2010\Debug\GIF.dll
+move out\vs2010\Debug\HTML5.dll.orig out\vs2010\Debug\HTML5.dll
+move out\vs2010\Debug\IO.dll.orig out\vs2010\Debug\IO.dll
+move out\vs2010\Debug\Unicode.dll.orig out\vs2010\Debug\Unicode.dll
+
+set PATH=%OLDPATH%
