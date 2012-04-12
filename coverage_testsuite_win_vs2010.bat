aa5ea890553d115d51ffb4deb13f6420e9324a11
Wolfgang Keller <wolfgangkeller@gmail.com>
2012-04-12 14:42:16 +0200

    Splitted file for running coverage test.

--- coverage_testsuite_win_vs2010.bat
+++ coverage_testsuite_win_vs2010.bat
@@ -18,13 +18,4 @@ out\vs2010\Debug\TestSuite.exe
 PAUSE
 VSPerfCmd.exe /SHUTDOWN
 
-move out\vs2010\Debug\TestSuite.exe.orig out\vs2010\Debug\TestSuite.exe
-move out\vs2010\Debug\Algorithm.dll.orig out\vs2010\Debug\Algorithm.dll
-move out\vs2010\Debug\BigNumber.dll.orig out\vs2010\Debug\BigNumber.dll
-move out\vs2010\Debug\DNS.dll.orig out\vs2010\Debug\DNS.dll
-move out\vs2010\Debug\GIF.dll.orig out\vs2010\Debug\GIF.dll
-move out\vs2010\Debug\HTML5.dll.orig out\vs2010\Debug\HTML5.dll
-move out\vs2010\Debug\IO.dll.orig out\vs2010\Debug\IO.dll
-move out\vs2010\Debug\Unicode.dll.orig out\vs2010\Debug\Unicode.dll
-
 set PATH=%OLDPATH%
---
+++ coverage_testsuite_win_vs2010_reset.bat
@@ -0,0 +1,9 @@
+
+move out\vs2010\Debug\TestSuite.exe.orig out\vs2010\Debug\TestSuite.exe
+move out\vs2010\Debug\Algorithm.dll.orig out\vs2010\Debug\Algorithm.dll
+move out\vs2010\Debug\BigNumber.dll.orig out\vs2010\Debug\BigNumber.dll
+move out\vs2010\Debug\DNS.dll.orig out\vs2010\Debug\DNS.dll
+move out\vs2010\Debug\GIF.dll.orig out\vs2010\Debug\GIF.dll
+move out\vs2010\Debug\HTML5.dll.orig out\vs2010\Debug\HTML5.dll
+move out\vs2010\Debug\IO.dll.orig out\vs2010\Debug\IO.dll
+move out\vs2010\Debug\Unicode.dll.orig out\vs2010\Debug\Unicode.dll
